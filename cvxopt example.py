36349ca0a9d6ac0670777f8ec7a4fb29f0351f6c
Is-there-a-name-that-still-available <93842552+Is-there-a-name-that-still-available@users.noreply.github.com>
2022-06-22 21:09:40 -0700

    Create cvxopt example.py

---
+++ cvxopt example.py
@@ -0,0 +1,31 @@
+# problem discription is in the Readme file
+
+import numpy as np
+import cvxopt
+
+n = 5 # dimenstion of x
+A = np.random.randn(n, n)
+b = np.random.randn(n)
+l_i = -0.5 
+u_i = 0.5
+
+# Form P,q,G,h matrices.
+# Write your code here:
+AT = np.transpose(A)
+P = 2*np.dot(AT,A)
+
+bt = np.transpose(b)
+Q = -2*np.dot(bt,A)
+q = np.transpose(Q)
+
+G = np.array([[-1,0,0,0,0],[0,-1,0,0,0],[0,0,-1,0,0],[0,0,0,-1,0],[0,0,0,0,-1],[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]])
+h = np.array([0.5,0.5,0.5,0.5,0.5,-0.5,-0.5,-0.5,-0.5,-0.5])
+
+P = cvxopt.matrix(P, tc='d')
+q = cvxopt.matrix(q, tc='d')
+G = cvxopt.matrix(G, tc='d')
+h = cvxopt.matrix(h, tc='d')
+sol = cvxopt.solvers.qp(P,q,G,h)
+
+print('x*=', sol['x'])
+print('p*=', sol['primal objective'])
